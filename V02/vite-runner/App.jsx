527df537dd24f19da15d7f680c12b24cc0bb7c51
guqianfeng <fred_gqf0221@163.com>
2024-01-13 12:55:53 +0800

    feat: 使用jsx

---
+++ V02/vite-runner/App.jsx
@@ -0,0 +1,27 @@
+import React from './core/React.js';
+// const App = React.createElement('div', { id: 'app' },
+//     React.createElement('div', null, 'heihei'),
+//     React.createElement('div', null, 'haha'),
+//     React.createElement('div', null, 'hehe')
+// )
+
+const App = (
+    <div id="app">
+        <div>jsx-heihei</div>
+        <div>jsx-haha</div>
+        <div>jsx-hehe</div>
+    </div>
+)
+
+// console.log(App)
+
+
+function AppTest () {
+    return (
+        <div id="test">app test</div>
+    )
+}
+console.log(AppTest);
+
+
+export default App
