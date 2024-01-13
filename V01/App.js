83c51551f77c433060a8df3e8ab43199bac10ce0
guqianfeng <fred_gqf0221@163.com>
2024-01-13 11:58:22 +0800

    v0.3和重构

---
+++ V01/App.js
@@ -0,0 +1,8 @@
+import React from './core/React.js';
+const App = React.createElement('div', { id: 'app' },
+    React.createElement('div', null, 'heihei'),
+    React.createElement('div', null, 'haha'),
+    React.createElement('div', null, 'hehe')
+)
+
+export default App
---
+++ V01/core/React.js
@@ -0,0 +1,45 @@
+function createTextNode(nodeValue) {
+    return {
+        type: 'TEXT_ELEMENT',
+        props: {
+            nodeValue,
+            children: []
+        }
+    }
+}
+
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child => typeof child === 'string' ? createTextNode(child) : child)
+        }
+    }
+}
+
+function render(el, container) {
+    // 1. 创建节点
+    // 2. 设置非children的属性
+    // 3. 处理children
+    // 4. 添加节点
+    const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(el.type)
+    Object.keys(el.props).forEach(key => {
+        if (key !== 'children') {
+            dom[key] = el.props[key]
+        }
+    })
+    const children = el.props.children
+    children.forEach(child => {
+        // 递归处理
+        render(child, dom)
+    })
+    container.append(dom)
+}
+
+const React = {
+    createElement,
+    render,
+}
+
+export default React
